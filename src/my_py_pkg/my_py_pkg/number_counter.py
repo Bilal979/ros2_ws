265b192d23c556d684c69d8b0f32b99ef9e7dbf1
Muhammad Bilal <bilalnisar369@gmail.com>
2026-09-18 02:02:46 +0000

    number counter reset service added

--- src/my_py_pkg/my_py_pkg/number_counter.py
+++ src/my_py_pkg/my_py_pkg/number_counter.py
@@ -2,17 +2,36 @@
 import rclpy
 from rclpy.node import Node
 from example_interfaces.msg import Int64
+from my_robot_interfaces.srv import ResetCounter
 
 class NumberCounterNode(Node):
     def __init__(self):
         super().__init__('number_counter')
-        self.counter_ = 0
-        self.number_subscriber_ = self.create_subscription(Int64, 'number', self.callback_number, 10)
+        self._counter = 0
+        self._number_subscriber = self.create_subscription(Int64, 'number', self.callback_number, 10)
+        self._reset_counter_service = self.create_service(ResetCounter, 'reset_counter', self.callback_reset_counter)
         self.get_logger().info('Number counter has started')
 
     def callback_number(self, msg:Int64):
-        self.counter_ += msg.data
-        self.get_logger().info(f"Counter: {self.counter_}")
+        self._counter += msg.data
+        self.get_logger().info(f"Counter: {self._counter}")
+
+    def callback_reset_counter(self, request:ResetCounter.Request, response:ResetCounter.Response):
+        
+        if request.reset_value < 0:
+            response.success = False
+            response.message = "Can not reset counter to a negative value"
+        elif request.reset_value > self._counter:
+            response.success = False
+            response.message = "Reset value must be less than counter value"
+        else:
+            self._counter = request.reset_value
+            self.get_logger().info(f"Reset counter to {self._counter}")
+            response.success = True
+            response.message = "Success"
+
+        return response
+
 
 
 def main(args=None):
--- src/my_py_pkg/my_py_pkg/number_publisher.py
+++ src/my_py_pkg/my_py_pkg/number_publisher.py
@@ -6,15 +6,15 @@ from example_interfaces.msg import Int64
 class NumberPublisherNode(Node):
     def __init__(self):
         super().__init__('number_publisher')
-        self.number_ = 2
-        self.number_publisher_ = self.create_publisher(Int64, 'number', 10)
-        self.number_timer_ = self.create_timer(1.0, self.publish_number)
+        self._number = 2
+        self._number_publisher = self.create_publisher(Int64, 'number', 10)
+        self._number_timer = self.create_timer(1.0, self.publish_number)
         self.get_logger().info('Number publisher has started')
 
     def publish_number(self):
         msg = Int64()
-        msg.data = self.number_
-        self.number_publisher_.publish(msg)
+        msg.data = self._number
+        self._number_publisher.publish(msg)
         
 
 
